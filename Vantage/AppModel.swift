6fb39303b421ef4d6d515f328de976ae480733ff
Terukuniac <thesynsize@gmail.com>
2025-07-03 10:54:10 +0900

    Initial Commit

---
+++ Vantage/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  Vantage
+//
+//  Created by Makoto Itoh on 2025/07/03.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
---
+++ VantageTests/VantageTests.swift
@@ -0,0 +1,17 @@
+//
+//  VantageTests.swift
+//  VantageTests
+//
+//  Created by Makoto Itoh on 2025/07/03.
+//
+
+import Testing
+@testable import Vantage
+
+struct VantageTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
